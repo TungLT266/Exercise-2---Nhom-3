0c5af75320708bff3f4492eb5f0280d3c89610e8
TungLT266 <37206096+TungLT266@users.noreply.github.com>
2018-03-09 20:07:24 +0700

    Thêm chức năng mở rộng

--- exercise2/app/src/main/java/tunglt/exercise2/ChangeTitleActivity.java
+++ exercise2/app/src/main/java/tunglt/exercise2/ChangeTitleActivity.java
@@ -6,83 +6,70 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ChangeTitleActivity extends AppCompatActivity {
+
+    public String background;
+
+    @BindView(R.id.editText) EditText editText;
+    @BindView(R.id.viewSample) View viewSample;
+
+    @OnClick(R.id.iv1)
+    public void onClick1(){
+        viewSample.setBackgroundColor(Color.parseColor("#D81B60"));
+    }
+
+    @OnClick(R.id.iv2)
+    public void onClick2(){
+        viewSample.setBackgroundColor(Color.parseColor("#8E24AA"));
+    }
+
+    @OnClick(R.id.iv3)
+    public void onClick3(){
+        viewSample.setBackgroundColor(Color.parseColor("#3949AB"));
+    }
+
+    @OnClick(R.id.iv4)
+    public void onClick4(){
+        viewSample.setBackgroundColor(Color.parseColor("#1E88E5"));
+    }
+
+    @OnClick(R.id.iv5)
+    public void onClick5(){
+        viewSample.setBackgroundColor(Color.parseColor("#00897B"));
+    }
+
+    @OnClick(R.id.iv6)
+    public void onClick6(){
+        viewSample.setBackgroundColor(Color.parseColor("#43A047"));
+    }
+
+    @OnClick(R.id.save)
+    public void onClickBtn(){
+        Intent changeTitleIntent = new Intent(ChangeTitleActivity.this, MainActivity.class);
+        changeTitleIntent.putExtra("textColor",((ColorDrawable) viewSample.getBackground()).getColor());
+        changeTitleIntent.putExtra("text", editText.getText().toString());
+        changeTitleIntent.putExtra("imageName", background);
+        startActivity(changeTitleIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_title);
 
-        final EditText editText = findViewById(R.id.editText);
-        final View viewSample = findViewById(R.id.viewSample);
-        final ImageView pinkView = findViewById(R.id.iv1);
-        ImageView purpleView = findViewById(R.id.iv2);
-        ImageView indigoView = findViewById(R.id.iv3);
-        ImageView blueView = findViewById(R.id.iv4);
-        ImageView tealView = findViewById(R.id.iv5);
-        ImageView greenView = findViewById(R.id.iv6);
+        ButterKnife.bind(ChangeTitleActivity.this);
 
         Intent intent = getIntent();
         String text = intent.getStringExtra("text");
         int textColor = intent.getIntExtra("textColor", 0);
         editText.setText(text);
         viewSample.setBackgroundColor(textColor);
-
-        pinkView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewSample.setBackgroundColor(Color.parseColor("#D81B60"));
-            }
-        });
-
-        purpleView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewSample.setBackgroundColor(Color.parseColor("#8E24AA"));
-            }
-        });
-
-        indigoView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewSample.setBackgroundColor(Color.parseColor("#3949AB"));
-            }
-        });
-
-        blueView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewSample.setBackgroundColor(Color.parseColor("#1E88E5"));
-            }
-        });
-
-        tealView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewSample.setBackgroundColor(Color.parseColor("#00897B"));
-            }
-        });
-
-        greenView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewSample.setBackgroundColor(Color.parseColor("#43A047"));
-            }
-        });
-
-        Button save = findViewById(R.id.save);
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            Intent changeTitleIntent = new Intent(ChangeTitleActivity.this, MainActivity.class);
-            changeTitleIntent.putExtra("textColor",((ColorDrawable) viewSample.getBackground()).getColor());
-            changeTitleIntent.putExtra("text", editText.getText().toString());
-            startActivity(changeTitleIntent);
-            }
-        });
+        background = intent.getStringExtra("imageName");
     }
 }
--- exercise2/app/src/main/java/tunglt/exercise2/MainActivity.java
+++ exercise2/app/src/main/java/tunglt/exercise2/MainActivity.java
@@ -1,22 +1,47 @@
 package tunglt.exercise2;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    public String backgroundDafault = "hn4";
+
+    @BindView(R.id.textView) TextView textView;
+    @BindView(R.id.backgroundImage)
+    ImageView backgroundImage;
+
+    @OnClick(R.id.changeTitle)
+    public void onButtonClick(){
+        Intent mainIntent = new Intent(MainActivity.this, ChangeTitleActivity.class);
+        mainIntent.putExtra("textColor", textView.getTextColors().getDefaultColor());
+        mainIntent.putExtra("text", textView.getText().toString());
+        mainIntent.putExtra("imageName", backgroundDafault);
+        startActivity(mainIntent);
+    }
+
+    @OnClick(R.id.changeBackground)
+    public void onClickBackground(){
+        Intent backgroundIntent = new Intent(MainActivity.this, ChangeBackgroundActivity.class);
+        backgroundIntent.putExtra("imageName", backgroundDafault);
+        backgroundIntent.putExtra("textColor", textView.getTextColors().getDefaultColor());
+        backgroundIntent.putExtra("text", textView.getText().toString());
+        startActivity(backgroundIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = findViewById(R.id.textView);
-//        String color = "#D81B60";
+        ButterKnife.bind(MainActivity.this);
 
         Intent intent = getIntent();
         if(intent != null){
@@ -29,19 +54,25 @@ public class MainActivity extends AppCompatActivity {
             if(color != 0){
                 textView.setTextColor(color);
             }
-        }
 
-//        textView.setTextColor(Color.parseColor(color));
-
-        Button changeTitle = findViewById(R.id.changeTitle);
-        changeTitle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mainIntent = new Intent(MainActivity.this, ChangeTitleActivity.class);
-                mainIntent.putExtra("textColor", textView.getTextColors().getDefaultColor());
-                mainIntent.putExtra("text", textView.getText().toString());
-                startActivity(mainIntent);
+            String background = intent.getStringExtra("imageName");
+            if(background != null){
+                switch (background){
+                    case "hn4": backgroundImage.setImageResource(R.drawable.hn4);
+                        break;
+                    case "hn2": backgroundImage.setImageResource(R.drawable.hn2);
+                        break;
+                    case "hn3": backgroundImage.setImageResource(R.drawable.hn3);
+                        break;
+                    case "hn5": backgroundImage.setImageResource(R.drawable.hn5);
+                        break;
+                    case "hn6": backgroundImage.setImageResource(R.drawable.hn6);
+                        break;
+                    case "hn7": backgroundImage.setImageResource(R.drawable.hn7);
+                        break;
+                }
+                backgroundDafault = background;
             }
-        });
+        }
     }
 }
